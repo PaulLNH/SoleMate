421285d82e8ef6b77c9ede4e87f5e24f83017d75
paullnh <plaird83@gmail.com>
2018-06-12 18:19:02 -0400

    Fixed the columns in the modal, added questions.txt for future implementation of dynamic question generation, Ran test on multiple users, Fixed bug that was preventing API link from working.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,9 +4,9 @@ module.exports = (app) => {
     // API GET Requests
     // Displays the JSON data of the friends database
 
-    // app.get("/api/friends", (req, res) => {
-    //     res.json(friendsData);
-    // });
+    app.get("/api/friends", (req, res) => {
+        res.json(friendsData);
+    });
 
     app.post("/api/friends", (req, res) => {
         // When the form is submited the data gets pushed to the friendsData array
